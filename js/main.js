32589a2089e2327221dca5812a239ad6f5f59790
Nina Zurbulaeva <naina148@mail.ru>
2021-01-30 23:43:34 +0300

    declaration arr and add method .slice()

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 let num = 266219;
 let multi = 1;
 
-arr = (String(num)).split('');
+let arr = (String(num)).split('');
 for (let i=0; i<arr.length; i++) {
     multi *= arr[i];
 }
 
 console.log(`Произведение цифр числа "${num}" равно ${multi}`);
 console.log("Полученное число в третьей степени равно", String(multi ** 3));
-console.log("Первая цифра - ", String(multi ** 3)[0]);
-console.log("Вторая цифра - ", String(multi ** 3)[1]);
+console.log("Первые две цифры - ", String(multi ** 3).slice(0,2));
